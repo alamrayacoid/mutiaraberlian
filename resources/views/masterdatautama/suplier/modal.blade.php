444ba2532aa51b1cebc6be6f71aa0589add74929
David Ridwan <ddavidridwan@gmail.com>
2019-02-25 18:08:07 -0800

    suplier done

--- resources/views/masterdatautama/suplier/modal.blade.php
+++ resources/views/masterdatautama/suplier/modal.blade.php
@@ -5,31 +5,11 @@
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header bg-gradient-info">
-        <h4 class="modal-title">Detail Data Armada</h4>
+        <h4 class="modal-title">Note</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
       <div class="modal-body">      
-        <div class="table-responsive">
-          <table class="table table-striped data-table table-hover" cellspacing="0">
-            <thead class="bg-primary">
-              <tr>
-                <th>Nama Barang</th>
-                <th>Harga</th>
-                <th>Stok</th>
-                <th>Keterangan</th>
-              </tr>
-            </thead>
-            <tbody>
-              
-               <tr>
-                <td><input type="text" class="form-control" value="Obat Masuk Angin"></td>
-                <td><input type="number" class="form-control" value="2500"></td>
-                <td><input type="number" class="form-control" value="20"></td>
-                <td><input type="text" class="form-control" value="Hampir Expired"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <textarea name="" id="" cols="30" rows="10" class="form-control form-control-sm" readonly="">Lorem ipsum dolor sit amet.</textarea>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
