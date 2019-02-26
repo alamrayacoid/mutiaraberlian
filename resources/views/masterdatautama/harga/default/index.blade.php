ee9d6b3e0ee9bcaf56b02bc93930c4a5893707dc
David Ridwan <ddavidridwan@gmail.com>
2019-02-26 02:25:37 -0800

    half done

--- resources/views/masterdatautama/harga/default/index.blade.php
+++ resources/views/masterdatautama/harga/default/index.blade.php
@@ -1,1 +1,52 @@
-belum
+<div class="tab-pane fade in show" id="default">
+	<div class="card">
+		<div class="card-header bordered p-2">
+			<div class="header-block">
+				<h3 class="title">Data Default</h3>
+			</div>
+		</div>
+		<div class="card-block">
+            <div class="row">
+                <div class="col-2">
+                    <label for="">Nama Barang</label>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-3">
+                    <select name="" id="" class="form-control form-control-sm select2">
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+            <section>
+            <div class="table-responsive">
+            <table class="table table-hover table-striped display nowrap" cellspacing="0" id="table_hargasatuan">
+                <thead class="bg-primary">
+                    <tr>
+                        <th width="10%">Satuan</th>
+                        <th width="60%">Nama</th>
+                        <th width="30%">Harga</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Satuan 1</td>
+                        <td><input type="text" class="form-control form-control-sm" readonly=""></td>
+                        <td><input type="text" class="form-control form-control-sm input-rupiah"></td>
+                    </tr>
+                    <tr>
+                        <td>Satuan 2</td>
+                        <td><input type="text" class="form-control form-control-sm" readonly=""></td>
+                        <td><input type="text" class="form-control form-control-sm input-rupiah"></td>
+                    </tr>
+                    <tr>
+                        <td>Satuan 3</td>
+                        <td><input type="text" class="form-control form-control-sm" readonly=""></td>
+                        <td><input type="text" class="form-control form-control-sm input-rupiah"></td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            </section>
+
+		</div>
+	</div>
+</div>
