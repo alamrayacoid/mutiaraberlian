e8789a9b0ec440faca23d89059ce43769f4784e3
ilhamroliz <ilham.rolis@gmail.com>
2019-02-27 11:34:02 +0700

    roliz

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -226,6 +226,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'CodeGenerator' => App\Http\Controllers\CodeGenerator::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
