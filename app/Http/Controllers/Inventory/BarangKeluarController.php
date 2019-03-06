3f1828bda7b759776afcb564464b6e59121b5f2e
Rizqon <m.rum.rowi.r@gmail.com>
2019-03-06 16:24:06 +0700

    update Agen: add photo

---
+++ app/Http/Controllers/Inventory/BarangKeluarController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Inventory;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BarangKeluarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return view('inventory/barangkeluar/index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      $company = DB::table('m_company')->select('c_id', 'c_name')->get();
+      $unit = DB::table('m_unit')->get();
+
+      $mutcat = DB::table('m_mutcat')->select('m_id', 'm_name')->where('m_name', 'like', 'Barang Keluar%')->get();
+
+
+      return view('inventory/barangkeluar/create')->with(compact('unit', 'company', 'mutcat'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      return view('inventory/barangkeluar/edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
