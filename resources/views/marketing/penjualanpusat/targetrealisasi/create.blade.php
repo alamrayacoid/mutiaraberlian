2fcd23016b212e5c0240d6a0c80206757dc41b35
David Ridwan <ddavidridwan@gmail.com>
2019-03-12 00:26:27 -0700

    too many update, so i cant expalain, if you wanna know, just call me 3 times

--- resources/views/marketing/penjualanpusat/targetrealisasi/create.blade.php
+++ resources/views/marketing/penjualanpusat/targetrealisasi/create.blade.php
@@ -25,7 +25,7 @@
 
                     <div class="card-header bordered p-2">
                       <div class="header-block pull-right">
-                        <a href="{{route('penjualanpusat.index')}}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i></a>
+                        <a href="{{route('order.index')}}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i></a>
                       </div>
                     </div>
 
@@ -34,24 +34,20 @@
                           
                           <div class="row">
                             
-                            <div class="col-md-2 col-sm-6 col-xs-12">
-                              <label>Bulan/Tahun</label>
-                            </div> 
+                          <div class="col-md-2 col-sm-6 col-xs-12">
+                            <label>Bulan/Tahun</label>
+                          </div> 
 
-                            <div class="col-md-10 col-sm-6 col-xs-12">
-                              <div class="form-group">
-                                <input type="text" class="form-control form-control-sm" id="datepicker" name="">
-                              </div>
+                          <div class="col-md-10 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                              <input type="text" class="form-control form-control-sm" id="datepicker" name="">
                             </div>
-
+                          </div>
                             <div class="container">
                                 <hr style="border:0.7px solid grey; margin-bottom:30px;">
-                          <div class="table-responsive">  
-                            <div class="pull-right mb-2">
-                               <button class="btn btn-primary btn-tambahb">Tamba Barang</button>
-                            </div>
+                          <div class="table-responsive">
 	                            <table class="table table-striped table-hover" cellspacing="0" id="table_target">
-	                                <thead class="bg-primary">
+                              <thead class="bg-primary">
 	                                    <tr>
 	                                    	<th width="8%">No</th>
 	                                		  <th width="30%">Kode/Nama Barang</th>
@@ -67,19 +63,19 @@
                                       <input type="text" class="form-control form-control-sm" value="1">
                                       </td>
 	                                		<td>
-                                      <input type="text" class="form-control form-control-sm">
+                                      <input type="text" class="form-control form-control-sm datepicker" value="">
                                       </td>
 	                                		<td>
-                                      <input type="text" class="form-control form-control-sm">
+                                      <input type="text" class="form-control form-control-sm input-rupiah" value="">
                                       </td>
 	                                		<td>
-                                      <input type="text" class="form-control form-control-sm">
-                                      </td>
-                                      <td>
-                                      <input type="text" class="form-control form-control-sm select2">
+                                      <input type="text" class="form-control form-control-sm datepicker" value="">
                                       </td>
 	                                		<td>
-                                      <button class="btn btn-danger btn-hapus btn-sm" type="button"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                                      <input type="text" class="form-control form-control-sm datepicker" value="">
+                                      </td>
+	                                		<td>
+                                      <button class="btn btn-success btn-tambah btn-sm" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
 	                                		</td>
 	                                	</tr>
 	                                </tbody>
@@ -92,7 +88,7 @@
                     </div>
                     <div class="card-footer text-right">
                       <button class="btn btn-primary btn-submit" type="button">Simpan</button>
-                      <a href="{{route('penjualanpusat.index')}}" class="btn btn-secondary">Kembali</a>
+                      <a href="{{route('order.index')}}" class="btn btn-secondary">Kembali</a>
                     </div>
                 </div>
 
@@ -109,32 +105,6 @@
 @section('extra_script')
 <script type="text/javascript">
   $(document).ready(function(){
-    $('#type_cus').change(function(){
-      if($(this).val() === 'kontrak'){
-        $('#label_type_cus').text('Jumlah Bulan');
-        $('#jumlah_hari_bulan').val('');
-        $('#pagu').val('');
-        $('#armada').prop('selectedIndex', 0).trigger('change');
-        $('.120mm').removeClass('d-none');
-        $('.125mm').addClass('d-none');
-        $('.122mm').removeClass('d-none');
-      } else if($(this).val() === 'harian'){
-        $('#label_type_cus').text('Jumlah Hari');
-        $('#armada').prop('selectedIndex', 0).trigger('change');
-        $('#pagu').val('');
-        $('#jumlah_hari_bulan').val('');
-        $('.122mm').addClass('d-none');
-        $('.120mm').removeClass('d-none');
-        $('.125mm').removeClass('d-none');
-      } else {
-        $('#jumlah_hari_bulan').val('');
-        $('#armada').prop('selectedIndex', 0).trigger('change');
-        $('#pagu').val('');
-        $('.122mm').addClass('d-none');
-        $('.120mm').addClass('d-none');
-        $('.125mm').addClass('d-none');
-      }
-    });
 
     $("#datepicker").datepicker( {
         format: "MM/yyyy",
@@ -142,21 +112,22 @@
         minViewMode: "months"
     });
 
+
     $(document).on('click', '.btn-hapus', function(){
       $(this).parents('tr').remove();
     });
 
-    $('.btn-tambahb').on('click',function(){
-      // var tbody = $(this).parents('tbody');
-      // var last_row = tbody.find('tr:last-child');
-      // var input = last_row.find('td:eq(0) input');
-      // var termin = input.val();
-      // termin = parseInt( termin );
-      // var next_termin = termin + 1;
+    $('.btn-tambah').on('click',function(){
+      var tbody = $(this).parents('tbody');
+      var last_row = tbody.find('tr:last-child');
+      var input = last_row.find('td:eq(0) input');
+      var termin = input.val();
+      termin = parseInt( termin );
+      var next_termin = termin + 1;
       $('#table_target')
       .append(
         '<tr>'+
-          '<td><input type="text" class="form-control form-control-sm" value=""></td>'+
+          '<td><input type="text" class="form-control form-control-sm" value="' + next_termin + '"></td>'+
           '<td><input type="text" class="form-control form-control-sm"></td>'+
           '<td><input type="text" class="form-control form-control-sm"></td>'+
           '<td><input type="text" class="form-control form-control-sm"></td>'+
