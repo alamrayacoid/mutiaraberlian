cdc936857a2ab3942399544060da8e0a9f0d83b9
Rizqon <m.rum.rowi.r@gmail.com>
2019-03-13 10:58:38 +0700

    update opname

--- app/Http/Controllers/Inventory/OpnameController.php
+++ app/Http/Controllers/Inventory/OpnameController.php
@@ -7,37 +7,31 @@ use App\Http\Controllers\Controller;
 
 use DB;
 use App\m_item;
-use App\d_opname;
+use App\d_opnameauth;
+use App\d_stock;
+use Validator;
 use CodeGenerator;
 use carbon\Carbon;
 
 class OpnameController extends Controller
 {
+
     /**
     * Return list of items from 'm_item'.
     *
     * @return \Illuminate\Http\Response
     */
-    public function getItems(Request $request)
+    public function getItemAutocomplete(Request $request)
     {
       $term = $request->term;
       $items = m_item::where('i_name', 'like', '%'.$term.'%')
-      ->orWhere('i_code', 'like', '%'.$term.'%')
-      ->with('getUnit1')
-      ->with('getUnit2')
-      ->with('getUnit3')
-      ->get();
+        ->orWhere('i_code', 'like', '%'.$term.'%')
+        ->get();
       if (sizeof($items) > 0) {
         foreach ($items as $item) {
           $results[] = [
             'id' => $item->i_id,
-            'label' => $item->i_name,
-            'unit1_id' => $items[0]->getUnit1['u_id'],
-            'unit1_name' => $items[0]->getUnit1['u_name'],
-            'unit2_id' => $items[0]->getUnit2['u_id'],
-            'unit2_name' => $items[0]->getUnit2['u_name'],
-            'unit3_id' => $items[0]->getUnit3['u_id'],
-            'unit3_name' => $items[0]->getUnit3['u_name']
+            'label' => $item->i_code .' - '. $item->i_name,
           ];
         }
       } else {
@@ -45,6 +39,66 @@ class OpnameController extends Controller
       }
       return response()->json($results);
     }
+
+    /**
+    * Return list of items from 'm_item'.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function getItem(Request $request)
+    {
+      $stock = d_stock::where('s_item', $request->itemId)
+        ->where('s_comp', $request->owner)
+        ->where('s_position', $request->position)
+        ->with('getItem')
+        ->with('getItem.getUnit1')
+        ->with('getItem.getUnit2')
+        ->with('getItem.getUnit3')
+        ->first();
+      if ($stock != null) {
+        $results = [
+          'unit1_id' => $stock->getItem['getUnit1']['u_id'],
+          'unit1_name' => $stock->getItem['getUnit1']['u_name'],
+          'unit2_id' => $stock->getItem['getUnit2']['u_id'],
+          'unit2_name' => $stock->getItem['getUnit2']['u_name'],
+          'unit3_id' => $stock->getItem['getUnit3']['u_id'],
+          'unit3_name' => $stock->getItem['getUnit3']['u_name']
+        ];
+      } else {
+        $results = 'empty';
+      }
+      return response()->json($results);
+    }
+
+    /**
+    * return total qty of select item in specific unit.
+    *
+    */
+    public function getQty(Request $request)
+    {
+      if ($request->itemId == null || $request->owner == null || $request->position == null) {
+        return response()->json([
+          'qty' => 0
+        ]);
+      }
+      $item = m_item::where('i_id', $request->itemId)
+        ->first();
+      $itemStock = d_stock::where('s_comp', $request->owner)
+        ->where('s_position', $request->position)
+        ->where('s_item', $request->itemId)
+        ->first();
+      if ($request->unit_sys == $item->i_unit1) {
+        $qty = $itemStock->s_qty / $item->i_unitcompare1;
+      } elseif ($request->unit_sys == $item->i_unit2) {
+        $qty = $itemStock->s_qty / $item->i_unitcompare2;
+      } elseif ($request->unit_sys == $item->i_unit3) {
+        $qty = $itemStock->s_qty / $item->i_unitcompare3;
+      }
+      return response()->json([
+        'qty' => $qty
+      ]);
+    }
+
 
     /**
     * Validate request before execute command.
@@ -107,7 +161,8 @@ class OpnameController extends Controller
      */
     public function create()
     {
-        return view('inventory/manajemenstok/opname/opnamestock/create');
+        $data['company'] = DB::table('m_company')->select('c_id', 'c_name')->get();
+        return view('inventory/manajemenstok/opname/opnamestock/create', compact('data'));
     }
 
     /**
@@ -130,16 +185,30 @@ class OpnameController extends Controller
       // insert data to db
       try {
         $nota = $this->getNewNota();
-        $newId = d_opname::max('o_id') + 1;
+        $newId = d_opnameauth::max('oa_id') + 1;
         DB::beginTransaction();
-          $opname = new d_opname;
-          $opname->o_id = $newId;
-          $opname->date = Carbon::now();
-          $opname->nota = $nota;
-          $opname->item = $request->itemId;
-          $opname->qty_real =
+          $opname = new d_opnameauth;
+          $opname->oa_id = $newId;
+          $opname->oa_date = Carbon::now();
+          $opname->oa_nota = $nota;
+          $opname->oa_item = $request->itemId;
+          $opname->oa_qtyreal = $request->qty_real;
+          $opname->oa_unitreal = $request->unit_real;
+          $opname->oa_qtysystem = $request->qty_sys;
+          $opname->oa_unitsystem = $request->unit_sys;
+          $opname->oa_status = 'P';
+          $opname->oa_insert = Carbon::now();
+          $opname->save();
+        DB::commit();
+        return response()->json([
+          'status' => 'berhasil'
+        ]);
       } catch (\Exception $e) {
-
+        DB::rollBack();
+        return response()->json([
+          'status' => 'gagal',
+          'message' => 'Gagal, hubungi pengembang !'
+        ]);
       }
 
     }
--- resources/views/inventory/manajemenstok/opname/opnamestock/create.blade.php
+++ resources/views/inventory/manajemenstok/opname/opnamestock/create.blade.php
@@ -9,7 +9,7 @@
       <p class="title-description">
         <i class="fa fa-home"></i>&nbsp;<a href="{{url('/home')}}">Home</a>
          / <span>Inventory</span>
-         / <a href="{{route('manajemenstok.index'')}}"><span>Pengelolaan Manajemen Stock</span></a>
+         / <a href="{{route('manajemenstok.index')}}"><span>Pengelolaan Manajemen Stock</span></a>
          / <span class="text-primary" style="font-weight: bold;"> Tambah Opname Stock </span>
        </p>
   </div>
@@ -27,87 +27,88 @@
                         <h3 class="title"> Tambah Opname Stock </h3>
                       </div>
                       <div class="header-block pull-right">
-                        <a href="{{route('opname.index')}}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i></a>
+                        <a href="{{ route('opname.index') }}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i></a>
                       </div>
                     </div>
 
-                    <div class="card-block">
+                    <form id="myForm" action="{{ route('opname.store') }}" method="post" autocomplete="off">
+                      <div class="card-block">
                         <section>
+                          <div class="row">
+                            <div class="col-md-2 col-sm-6 col-xs-12">
+                              <label>Nama Barang</label>
+                            </div>
+                            <div class="col-md-10 col-sm-6 col-xs-12">
+                              <div class="form-group">
+                                <input type="hidden" name="itemId" id="itemId">
+                                <input type="text" class="form-control form-control-sm" id="name" name="name" style="text-transform:uppercase">
+                              </div>
+                            </div>
 
-                            <div class="row">
+                            <div class="col-md-2 col-sm-6 col-xs-12">
+                              <label>Pemilik</label>
+                            </div>
+                            <div class="col-md-10 col-sm-6 col-xs-12">
+                              <div class="form-group">
+                                <select name="owner" id="owner" class="form-control form-control-sm select2">
+                                  @foreach($data['company'] as $position)
+                                  <option value="{{$position->c_id}}">{{$position->c_name}}</option>
+                                  @endforeach
+                                </select>
+                              </div>
+                            </div>
 
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Nama Barang</label>
-                                </div>
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                <div class="form-group">
-                                    <input type="hidden" name="itemId" id="itemId">
-                                    <input type="text" class="form-control form-control-sm" id="name" name="name" style="text-transform:uppercase">
-                                </div>
-                                </div>
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Pemilik</label>
-                                </div>
-
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                <div class="form-group">
-                                  <select name="" id="" class="form-control form-control-sm">
-                                    <option value=""></option>
-                                  </select>
-                                </div>
-                                </div>
-
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Lokasi</label>
-                                </div>
-
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                <div class="form-group">
-                                  <select name="" id="" class="form-control form-control-sm">
-                                    <option value=""></option>
-                                  </select>
-                                </div>
-                                </div>
+                            <div class="col-md-2 col-sm-6 col-xs-12">
+                              <label>Lokasi</label>
+                            </div>
+                            <div class="col-md-10 col-sm-6 col-xs-12">
+                              <div class="form-group">
+                                <select name="position" id="position" class="form-control form-control-sm select2">
+                                  @foreach($data['company'] as $position)
+                                  <option value="{{$position->c_id}}">{{$position->c_name}}</option>
+                                  @endforeach
+                                </select>
+                              </div>
+                            </div>
                             <div class="col-12"><hr></div>
                             <div class="col-md-6">
-                                <div class="title-block">
-                                    <h3 class="title"> Stock System </h3>
+                              <div class="title-block">
+                                <h3 class="title"> Stock System </h3>
+                              </div>
+                              <form role="form">
+                                <div class="form-group">
+                                  <label class="control-label" for="unit_sys">Satuan</label>
+                                  <select type="text" class="form-control form-control-sm select2" id="unit_sys" name="unit_sys">
+                                    <option value="" selected disabled>Pilih Satuan</option>
+                                  </select>
                                 </div>
-                                <form role="form">
-                                    <div class="form-group">
-                                        <label class="control-label" for="unit_sys">Satuan</label>
-                                        <select type="text" class="form-control form-control-sm select2" id="unit_sys" name="unit_sys">
-                                            <option value="" disabled>Pilih Satuan</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label" for="qty_sys">Qty</label>
-                                        <input type="number" class="form-control form-control-sm" id="qty_sys" name="qty_sys" readonly>
-                                    </div>
-                                </form>
+                                <div class="form-group">
+                                  <label class="control-label" for="qty_sys">Qty</label>
+                                  <input type="text" class="form-control form-control-sm" id="qty_sys" name="qty_sys" readonly>
+                                </div>
+                              </form>
                             </div>
                             <div class="col-md-6">
-                                <div class="title-block">
-                                    <h3 class="title"> Stock Real </h3>
-                                </div>
-                                <form role="form">
-                                    <div class="form-group">
-                                        <label class="control-label" for="unit_real">Satuan</label>
-                                        <select type="text" class="form-control form-control-sm select2" id="unit_real" name="unit_real">
-                                            <option value="" disabled>Pilih Satuan</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label" for="qty_real">Qty</label>
-                                        <input type="number" class="form-control form-control-sm" id="qty_real" name="qty_real">
-                                    </div>
-                                </form>
+                              <div class="title-block">
+                                <h3 class="title"> Stock Real </h3>
+                              </div>
+                              <div class="form-group">
+                                <label class="control-label" for="unit_real">Satuan</label>
+                                <select type="text" class="form-control form-control-sm select2" id="unit_real" name="unit_real">
+                                  <option value="" selected disabled>Pilih Satuan</option>
+                                </select>
+                              </div>
+                              <div class="form-group">
+                                <label class="control-label" for="qty_real">Qty</label>
+                                <input type="number" class="form-control form-control-sm" id="qty_real" name="qty_real">
+                              </div>
                             </div>
-                            </div>
+                          </div>
                         </section>
-                    </div>
+                      </div>
+                    </form>
                     <div class="card-footer text-right">
-                      <button class="btn btn-primary btn-submit" type="button">Simpan</button>
+                      <button class="btn btn-primary btn-submit" type="button" id="btn_simpan">Simpan</button>
                       <a href="{{route('mngagen.index')}}" class="btn btn-secondary">Kembali</a>
                     </div>
                 </div>
@@ -131,66 +132,137 @@
   });
 
   $(document).ready(function(){
-
     $('#name').autocomplete({
-      source: baseUrl+'/inventory/manajemenstok/opnamestock/getItems',
+      source: baseUrl+'/inventory/manajemenstok/opnamestock/getItemAutocomplete',
       minLength: 2,
       select: function(event, data){
         $('#itemId').val(data.item.id);
-        $('#unit_sys').find('option').not(':first').remove();
-        if (data.item.unit1_id != null) {
-          $('#unit_sys').append('<option value="'+ data.item.unit1_id +'" data-qty="">'+ data.item.unit1_name +'</option>');
-          $('#unit_real').append('<option value="'+ data.item.unit1_id +'" data-qty="">'+ data.item.unit1_name +'</option>');
-        }
-        if (data.item.unit2_id != null) {
-          $('#unit_sys').append('<option value="'+ data.item.unit2_id +'" data-qty="">'+ data.item.unit2_name +'</option>');
-          $('#unit_real').append('<option value="'+ data.item.unit2_id +'" data-qty="">'+ data.item.unit2_name +'</option>');
-        }
-        if (data.item.unit3_id != null) {
-          $('#unit_sys').append('<option value="'+ data.item.unit3_id +'" data-qty="">'+ data.item.unit3_name +'</option>');
-          $('#unit_real').append('<option value="'+ data.item.unit3_id +'" data-qty="">'+ data.item.unit3_name +'</option>');
-        }
+        updateUnitSys();
       }
     });
 
-    $('#unit_sys').on('select2:select', function() {
+    $('#owner').on('select2:select', function() {
+      updateUnitSys();
+    });
 
+    $('#position').on('select2:select', function() {
+      updateUnitSys();
+    });
+
+    $('#unit_sys').on('select2:select', function() {
+      $.ajax({
+        type: 'get',
+        data: $('#myForm').serialize(),
+        url: baseUrl + '/inventory/manajemenstok/opnamestock/getQty',
+        success: function(data) {
+          $('#qty_sys').val('');
+          $('#qty_sys').val(data.qty);
+        }
+      });
     })
 
-    $(document).on('click', '.btn-submit', function(){
-		var ini = $(this);
-		$.confirm({
-			animation: 'RotateY',
-			closeAnimation: 'scale',
-			animationBounce: 1.5,
-			icon: 'fa fa-exclamation-triangle',
-			title: 'Peringatan!',
-			content: 'Apa anda sudah yakin?',
-			theme: 'disable',
-			buttons: {
-				info: {
-					btnClass: 'btn-blue',
-					text:'Ya',
-					action : function(){
-                        $.toast({
-                        heading: 'Success',
-                        text: 'Data Berhasil di Simpan',
-                        bgColor: '#00b894',
-                        textColor: 'white',
-                        loaderBg: '#55efc4',
-                        icon: 'success'
-                    })
-					}
-				},
-				cancel:{
-					text: 'Tidak',
-					action: function () {
-						// tutup confirm
-					}
-				}
-			}
-		});
-	});
+    $('#btn_simpan').on('click', function() {
+      SubmitForm(event);
+    });
+
+    // $(document).on('click', '.btn-submit', function(){
+    //   var ini = $(this);
+  	// 	$.confirm({
+  	// 		animation: 'RotateY',
+  	// 		closeAnimation: 'scale',
+  	// 		animationBounce: 1.5,
+  	// 		icon: 'fa fa-exclamation-triangle',
+  	// 		title: 'Peringatan!',
+  	// 		content: 'Apa anda sudah yakin?',
+  	// 		theme: 'disable',
+  	// 		buttons: {
+  	// 			info: {
+  	// 				btnClass: 'btn-blue',
+  	// 				text:'Ya',
+  	// 				action : function(){
+    //                       $.toast({
+    //                       heading: 'Success',
+    //                       text: 'Data Berhasil di Simpan',
+    //                       bgColor: '#00b894',
+    //                       textColor: 'white',
+    //                       loaderBg: '#55efc4',
+    //                       icon: 'success'
+    //                   })
+  	// 				}
+  	// 			},
+  	// 			cancel:{
+  	// 				text: 'Tidak',
+  	// 				action: function () {
+  	// 					// tutup confirm
+  	// 				}
+  	// 			}
+  	// 		}
+  	// 	});
+  	// });
+    //
   });
+
+  function updateUnitSys()
+  {
+    $.ajax({
+      type: 'get',
+      data: $('#myForm').serialize(),
+      url: baseUrl + '/inventory/manajemenstok/opnamestock/getItem',
+      success: function(data) {
+        $('#unit_sys').find('option').not(':first').remove();
+        $('#unit_real').find('option').not(':first').remove();
+        $('#qty_sys').val('');
+        $('#qty_real').val('');
+        if (data != 'empty') {
+          if (data.unit1_id != null) {
+            $('#unit_sys').append('<option value="'+ data.unit1_id +'" data-qty="">'+ data.unit1_name +'</option>');
+            $('#unit_real').append('<option value="'+ data.unit1_id +'" data-qty="">'+ data.unit1_name +'</option>');
+          }
+          if (data.unit2_id != null) {
+            $('#unit_sys').append('<option value="'+ data.unit2_id +'" data-qty="">'+ data.unit2_name +'</option>');
+            $('#unit_real').append('<option value="'+ data.unit2_id +'" data-qty="">'+ data.unit2_name +'</option>');
+          }
+          if (data.unit3_id != null) {
+            $('#unit_sys').append('<option value="'+ data.unit3_id +'" data-qty="">'+ data.unit3_name +'</option>');
+            $('#unit_real').append('<option value="'+ data.unit3_id +'" data-qty="">'+ data.unit3_name +'</option>');
+          }
+        } else {
+          messageWarning('Perhatian', 'Data tidak ditemukan !');
+        }
+      }
+    });
+  }
+
+  // submit form to store data in db
+  function SubmitForm(event)
+  {
+    loadingShow();
+    event.preventDefault();
+    form_data = $('#myForm').serialize();
+
+    $.ajax({
+      data : form_data,
+      type : "post",
+      url : $("#myForm").attr('action'),
+      dataType : 'json',
+      success : function (response){
+        if(response.status == 'berhasil'){
+          loadingHide();
+          messageSuccess('Berhasil', 'Data berhasil ditambahkan !');
+          location.reload();
+        } else if (response.status == 'invalid') {
+          loadingHide();
+          messageFailed('Perhatian', response.message);
+        } else if (response.status == 'gagal') {
+          loadingHide();
+          messageWarning('Error', response.message);
+        }
+      },
+      error : function(e){
+        loadingHide();
+        messageWarning('Gagal', 'Data gagal ditambahkan, hubungi pengembang !');
+      }
+    })
+  }
 </script>
 @endsection
