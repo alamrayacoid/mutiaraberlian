571b31f4a342f8778fa1f9a91da0e4feefa9b14d
ilhamroliz <ilham.rolis@gmail.com>
2019-03-14 10:33:55 +0700

    roliz

--- app/Http/Controllers/pushotorisasiController.php
+++ app/Http/Controllers/pushotorisasiController.php
@@ -1,41 +1,25 @@
 <?php
-namespace App\Http\Controllers;
-use Illuminate\Http\Request;
 
-class OtorisasiController extends Controller
-{
-    public function otorisasi(){
-        return view('notifikasiotorisasi.otorisasi.index');
-    }
-    public function perubahanhargajual(){
-        return view('notifikasiotorisasi.otorisasi.perubahanhargajual.index');
-    }
-    public function pengeluaranlebih(){
-        return view('notifikasiotorisasi.otorisasi.pengeluaranlebih.index');
-    }
-    public function opname_otorisasi(){
-        return view('notifikasiotorisasi.otorisasi.opname.index');
-    }
-    public function adjustment(){
-        return view('notifikasiotorisasi.otorisasi.adjustment.index');
-    }
-    public function revisi(){
-        return view('notifikasiotorisasi.otorisasi.revisi.index');
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 use Pusher;
 use DB;
+
 class pushotorisasiController extends Controller
 {
-    static function otorisasiup(){
-      $options = array(
-        'cluster' => env('PUSHER_APP_CLUSTER'),
-        'useTLS' => true
-      );
-      $pusher = new Pusher(
-        env('PUSHER_APP_KEY'),
-        env('PUSHER_APP_SECRET'),
-        env('PUSHER_APP_ID'),
-        $options
-      );
-      $pusher->trigger('my-channel', 'my-event', true);
+    static function otorisasiup()
+    {
+        $options = array(
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'useTLS' => true
+        );
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            $options
+        );
+        $pusher->trigger('my-channel', 'my-event', true);
     }
 }
