d8ddfab2729beba90c19e14435e11ad9073e24d0
David Ridwan <ddavidridwan@gmail.com>
2019-03-13 21:37:15 -0700

    syudah lagi

--- resources/views/inventory/distribusibarang/history/index.blade.php
+++ resources/views/inventory/distribusibarang/history/index.blade.php
@@ -1,13 +1,42 @@
 <div class="tab-pane fade in show" id="historybarang">
-	<div class="card">
-		<div class="card-header bordered p-2">
-			<div class="header-block">
-				<h3 class="title">History Distribusi Barang</h3>
-			</div>
-			<div class=""></div>
-		</div>
-		<div class="card-block">
-			<section>
+    <div class="card">
+        <div class="card-header bordered p-2">
+            <div class="header-block">
+                <h3 class="title">History Distribusi Barang</h3>
+            </div>
+            <div class=""></div>
+        </div>
+        <div class="card-block">
+            <section>
+                <fieldset class="mb-3">
+                    <div class="row">
+
+                        <div class="col-md-2 col-sm-6 col-xs-12">
+                            <label>Tanggal Awal</label>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm datepicker" id="rekrut_from" name="">
+                            </div>
+                        </div>
+
+                        <div class="col-md-2 col-sm-6 col-xs-12">
+                            <label>Tanggal Akhir</label>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm datepicker" id="rekrut_to" name="">
+                            </div>
+                        </div>
+
+                        <div class="col-2">
+                            <button class="btn btn-sm btn-primary"><i class="fa fa-search"></i></button>
+                        </div>
+
+                    </div>
+                </fieldset>
                 <div class="table-responsive">
                     <table class="table table-hover table-striped display nowrap" cellspacing="0" id="table_history">
                         <thead class="bg-primary">
@@ -38,7 +67,7 @@
                         </tbody>
                     </table>
                 </div>
-			</section>
-		</div>
-	</div>
+            </section>
+        </div>
+    </div>
 </div>
