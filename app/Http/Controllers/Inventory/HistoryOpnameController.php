9563d302ce588f9481d85b2931e38bc9586d5490
Rizqon <m.rum.rowi.r@gmail.com>
2019-03-15 19:38:20 +0700

    up

--- app/Http/Controllers/Inventory/HistoryOpnameController.php
+++ app/Http/Controllers/Inventory/HistoryOpnameController.php
@@ -34,15 +34,11 @@ class HistoryOpnameController extends Controller
         return '<td>'. $datas->i_name .'</td>';
       })
       ->addColumn('status', function($datas) {
-<<<<<<< HEAD
-        return '<td><button class="btn btn-primary status-pending" style="pointer-events: none">-</button></td>';
-=======
         if ($datas->o_status == 'P') {
             return '<td><button class="btn btn-primary status-pending" style="pointer-events: none">Pending</button></td>';
         } elseif ($datas->o_status == 'D') {
           return '<td><button class="btn btn-primary status-approve" style="pointer-events: none">Sudah Digunakan</button></td>';
         }
->>>>>>> 4e7e672de29fe6cb1e82c5f64af0081af3804a50
       })
       ->rawColumns(['date', 'name', 'status'])
       ->make(true);
