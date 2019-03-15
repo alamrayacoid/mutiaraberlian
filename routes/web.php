89aecd51d71b9a7d27e67f10efafec3544dee480
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-03-15 14:39:28 +0700

    mutiara

--- routes/web.php
+++ routes/web.php
@@ -161,6 +161,8 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/produksi/penerimaanbarang/getnotapo', 'PenerimaanProduksiController@getNotaPO');
     Route::get('/produksi/penerimaanbarang/detailitem', 'PenerimaanProduksiController@getProduksiDetailItem')->name('penerimaan.detailitem');
     Route::get('/produksi/penerimaanbarang/detailtermin', 'PenerimaanProduksiController@getProduksiDetailTermin')->name('penerimaan.detailtermin');
+    Route::get('/produksi/penerimaanbarang/terima-barang/{id}', 'PenerimaanProduksiController@terimaBarang')->name('penerimaan.terimabarang');
+    Route::get('/produksi/penerimaanbarang/getlistitem', 'PenerimaanProduksiController@listTerimaBarang')->name('penerimaan.listterimabarang');
     Route::get('/produksi/penerimaanbarang/create', 'PenerimaanProduksiController@create_penerimaan_barang')->name('penerimaan.create');
     // Pembayaran
     Route::get('/produksi/pembayaran/index', 'ProduksiController@pembayaran')->name('pembayaran.index');
