a56ad9b559728b7ce7c94a1168fa971e11f3604e
David Ridwan <ddavidridwan@gmail.com>
2019-03-20 00:53:18 -0700

    responsive

--- resources/views/produksi/pembayaran/bayar.blade.php
+++ resources/views/produksi/pembayaran/bayar.blade.php
@@ -13,80 +13,80 @@
                     <input type="hidden" name="poid" id="poid">
                     <input type="hidden" name="terminid" id="terminid">
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Nota</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-sm-12 col-md-4">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: left;"
                                    id="nota" name="nota" value="PO-001/13/03/2019" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Supplier</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-sm-12 col-md-4">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: left;"
                                    id="supplier" name="supplier" value="EFENDI" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Tanggal Pembelian</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-md-4 col-sm-12">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: left;"
                                    id="tgl_beli" name="tgl_beli" value="13/03/2019" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Termin ke-</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-md-4 col-sm-12">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: left;"
                                    id="termin" name="termin" value="1" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Tagihan</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-md-4 col-sm-12">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: right;"
                                    id="tagihan" name="tagihan" value="Rp. 435.000" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;" id="nominal_termin_lbl">Terbayar</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-md-4 col-sm-12">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: right;"
                                    id="terbayar" name="terbayar" value="Rp. 0" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Kekurangan</label>
                         </div>
-                        <div class="col-4">
+                        <div class="col-md-4 col-sm-12">
                             <input type="text" class="form-control-plaintext"
                                    style="font-weight: bold; text-align: right;"
                                    id="kekurangan" name="kekurangan" readonly value="Rp. 0">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-3 col-sm-4">
+                        <div class="col-lg-3 col-sm-8">
                             <label for="" style="margin-right:10px;">Nilai Bayar</label>
                         </div>
-                        <div class="col-4 mb-3">
+                        <div class="col-md-4 mb-3 col-sm-12">
                             <input type="text" class="form-control form-control-sm input-rupiah" value="Rp. 0"
                                    id="nilai_bayar" name="nilai_bayar">
                             {{--<div class="input-group">--}}
