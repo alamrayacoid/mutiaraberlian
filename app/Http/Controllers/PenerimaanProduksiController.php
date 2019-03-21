1ab262a22a481f6ea834d7ca70292d0796b5b076
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-03-21 14:20:37 +0700

    mutiara

--- app/Http/Controllers/PenerimaanProduksiController.php
+++ app/Http/Controllers/PenerimaanProduksiController.php
@@ -26,16 +26,14 @@ class PenerimaanProduksiController extends Controller
     public function getNotaPO()
     {
         $data = DB::table('d_productionorder')
-            ->join('d_productionorderdt', 'pod_productionorder', '=', 'po_id')
             ->join('m_supplier', 's_id', '=', 'po_supplier')
             ->leftjoin('d_itemreceipt', 'ir_notapo', '=', 'po_nota')
             ->leftjoin('d_itemreceiptdt', function($x){
                 $x->on('ird_itemreceipt', '=', 'ir_id');
-                $x->on('ird_item', '=', 'pod_item');
             })
             ->where('d_productionorder.po_status', '=', 'BELUM')
             ->groupBy('po_id')
-            ->select('po_id', 'po_nota', 's_name', 'po_date', DB::raw('SUM(pod_qty) AS qty_order'), DB::raw('SUM(ird_qty) AS diterima'))
+            ->select('po_id', 'po_nota', 's_name', 'po_date')
             ->get();
 
         return DataTables::of($data)
