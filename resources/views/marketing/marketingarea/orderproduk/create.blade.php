c26fefb07df057ba3245057c16ece7d8a41b5d62
masumam06 <aku.masumam@gmail.com>
2019-03-21 16:20:54 +0700

    umam

--- resources/views/marketing/marketingarea/orderproduk/create.blade.php
+++ resources/views/marketing/marketingarea/orderproduk/create.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 <article class="content animated fadeInLeft">
-
     <div class="title-block text-primary">
         <h1 class="title"> Tambah Data Order Produk ke Cabang</h1>
         <p class="title-description">
@@ -13,15 +12,10 @@
             / <span class="text-primary" style="font-weight: bold;"> Tambah Data Order Produk ke Cabang </span>
         </p>
     </div>
-
     <section class="section">
-
         <div class="row">
-
             <div class="col-12">
-
                 <div class="card">
-
                     <div class="card-header bordered p-2">
                         <div class="header-block">
                             <h3 class="title"> Tambah Data Order Produk ke Cabang </h3>
@@ -30,118 +24,111 @@
                             <a href="{{route('marketingarea.index')}}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i></a>
                         </div>
                     </div>
-
                     <div class="card-block">
                         <form action="" id="formOrder">
-                        <section>
-                        
-                            <div class="row">
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Area</label>
-                                </div>
-
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                    <div class="row">
-                                        <div class="form-group col-6">
-                                            <select name="po_prov" id="prov" class="form-control form-control-sm select2" onchange="getProvId()">
-                                                <option value="" selected="" disabled="">=== Pilih Provinsi ===</option>
-                                                @foreach($provinsi as $prov)
+                            <section>
+                                
+                                <div class="row">
+                                    <div class="col-md-2 col-sm-6 col-xs-12">
+                                        <label>Area</label>
+                                    </div>
+                                    <div class="col-md-10 col-sm-6 col-xs-12">
+                                        
+                                        <div class="row">
+                                            <div class="form-group col-6">
+                                                <select name="po_prov" id="prov" class="form-control form-control-sm select2" onchange="getProvId()">
+                                                    <option value="" selected="" disabled="">=== Pilih Provinsi ===</option>
+                                                    @foreach($provinsi as $prov)
                                                     <option value="{{$prov->wp_id}}">{{$prov->wp_name}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-6">
+                                                <select name="po_city" id="city" class="form-control form-control-sm select2 city" onchange="getCityId()">
+                                                    <option value="" selected disabled>=== Pilih Kota ===</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-sm-6 col-xs-12">
+                                        <label>Agen Pembeli</label>
+                                    </div>
+                                    <div class="col-md-10 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <select name="po_agen[]" id="agen" class="form-control form-control-sm select2 agen">
+                                                <option value="" selected disabled>=== Pilih Agen ===</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-sm-6 col-xs-12">
+                                        <label>Cabang</label>
+                                    </div>
+                                    <div class="col-md-10 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <select name="po_comp[]" id="comp" class="form-control form-control-sm select2">
+                                                <option value="" selected="" disabled="">=== Pilih Cabang ===</option>
+                                                @foreach($company as $comp)
+                                                <option value="{{$comp->c_id}}">{{$comp->c_name}}</option>
                                                 @endforeach
                                             </select>
                                         </div>
-                                        <div class="form-group col-6">
-                                            <select name="po_city" id="city" class="form-control form-control-sm select2 city" onchange="getCityId()">
-                                                <option value="" selected disabled>=== Pilih Kota ===</option>
-                                            </select>
+                                    </div>
+                                    <div class="col-md-2 col-sm-6 col-xs-12">
+                                        <label>Total Harga</label>
+                                    </div>
+                                    <div class="col-md-10 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-sm"
+                                            name="total_harga[]" id="total_harga" readonly>
+                                            <input type="hidden" name="tot_hrg[]" id="tot_hrg">
                                         </div>
                                     </div>
-                                </div>
-
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Agen Pembeli</label>
-                                </div>
-
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                    <div class="form-group">
-                                        <select name="po_agen[]" id="agen" class="form-control form-control-sm select2 agen">
-                                            <option value="" selected disabled>=== Pilih Agen ===</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Cabang</label>
-                                </div>
-
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                    <div class="form-group">
-                                        <select name="po_comp[]" id="comp" class="form-control form-control-sm select2">
-                                            <option value="" selected="" disabled="">=== Pilih Cabang ===</option>
-                                            @foreach($company as $comp)
-                                                <option value="{{$comp->c_id}}">{{$comp->c_name}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2 col-sm-6 col-xs-12">
-                                    <label>Total Harga</label>
-                                </div>
-
-                                <div class="col-md-10 col-sm-6 col-xs-12">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control form-control-sm"
-                                               name="total_harga[]" id="total_harga" readonly>
-                                        <input type="hidden" name="tot_hrg[]" id="tot_hrg">
-                                    </div>
-                                </div>
-                                <div class="table-responsive">
-                                    <table class="table table-striped table-hover" cellspacing="0" id="table_order">
-                                        <thead class="bg-primary">
-                                            <tr>
-                                                <th width="30%">Kode Barang/Nama Barang</th>
-                                                <th width="10%">Satuan</th>
-                                                <th width="10%">Jumlah</th>
-                                                <th width="25%">Harga Satuan</th>
-                                                <th width="25%">Sub Total</th>
-                                                <th>Aksi</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>
-                                                    <input type="text" name="barang[]"
-                                                                   class="form-control form-control-sm barang"
-                                                                   style="text-transform:uppercase">
-                                                    <input type="hidden" name="idItem[]" class="itemId">
-                                                    <input type="hidden" name="kode[]" class="kode">
-                                                </td>
-                                                <td>                                                    
-                                                    <select name="po_unit[]"
+                                    <div class="table-responsive">
+                                        <table class="table table-striped table-hover" cellspacing="0" id="table_order">
+                                            <thead class="bg-primary">
+                                                <tr>
+                                                    <th width="30%">Kode Barang/Nama Barang</th>
+                                                    <th width="10%">Satuan</th>
+                                                    <th width="10%">Jumlah</th>
+                                                    <th width="25%">Harga Satuan</th>
+                                                    <th width="25%">Sub Total</th>
+                                                    <th>Aksi</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+                                                        <input type="text" name="barang[]"
+                                                        class="form-control form-control-sm barang"
+                                                        style="text-transform:uppercase">
+                                                        <input type="hidden" name="idItem[]" class="itemId">
+                                                        <input type="hidden" name="kode[]" class="kode">
+                                                    </td>
+                                                    <td>
+                                                        <select name="po_unit[]"
                                                             class="form-control form-control-sm select2 satuan">
-                                                    </select>
-                                                </td>
-                                                <td>
-                                                    <input type="number" name="po_qty[]" min="0" class="form-control form-control-sm jumlah" value="0">
-                                                </td>
-                                                <td>
-                                                    <input type="text" name="po_harga[]" class="form-control form-control-sm input-rupiah harga" value="Rp. 0">
-                                                    <input type="hidden" name="po_hrg[]" class="po_hrg">
-                                                </td>
-                                                <td>                                                    
-                                                    <input type="text" name="subtotal[]" style="text-align: right;" class="form-control form-control-sm subtotal" readonly>
-                                                    <input type="hidden" name="sbtotal[]" class="sbtotal">
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-success btn-tambah-order btn-sm rounded-circle" style="color:white;" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                                                        </select>
+                                                    </td>
+                                                    <td>
+                                                        <input type="number" name="po_qty[]" min="0" class="form-control form-control-sm jumlah" value="0">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" name="po_harga[]" class="form-control form-control-sm input-rupiah harga" value="Rp. 0">
+                                                        <input type="hidden" name="po_hrg[]" class="po_hrg">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" name="subtotal[]" style="text-align: right;" class="form-control form-control-sm subtotal" readonly>
+                                                        <input type="hidden" name="sbtotal[]" class="sbtotal">
+                                                    </td>
+                                                    <td>
+                                                        <button class="btn btn-success btn-tambah-order btn-sm rounded-circle" style="color:white;" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
-                            </div>
-                        </section>
+                            </section>
                         </form>
                     </div>
                     <div class="card-footer text-right">
@@ -149,13 +136,9 @@
                         <a href="{{route('marketingarea.index')}}" class="btn btn-secondary">Kembali</a>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </section>
-
 </article>
 
 @endsection
@@ -177,6 +160,7 @@
             idxBarang = $('.barang').index(this);
             setArrayCode();
         });
+
 
         $(".barang").eq(idxBarang).on("keyup", function () {
             $(".itemId").eq(idxBarang).val('');
