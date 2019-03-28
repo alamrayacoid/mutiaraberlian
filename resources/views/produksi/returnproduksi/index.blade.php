f36c5b738edaac0a4706f35daa62e707206042ee
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-03-28 11:37:30 +0700

    mutiara

--- resources/views/produksi/returnproduksi/index.blade.php
+++ resources/views/produksi/returnproduksi/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+    @include('produksi.returnproduksi.detail_return')
+
 <article class="content animated fadeInLeft">
 
 	<div class="title-block text-primary">
@@ -84,58 +86,18 @@
             ],
         });
 
-		$(document).on('click', '.btn-disable', function(){
-			var ini = $(this);
-			$.confirm({
-				animation: 'RotateY',
-				closeAnimation: 'scale',
-				animationBounce: 1.5,
-				icon: 'fa fa-exclamation-triangle',
-				title: 'Peringatan!',
-				content: 'Apa anda yakin mau menonaktifkan data ini?',
-				theme: 'disable',
-			    buttons: {
-			        info: {
-						btnClass: 'btn-blue',
-			        	text:'Ya',
-			        	action : function(){
-							$.toast({
-								heading: 'Information',
-								text: 'Data Berhasil di Nonaktifkan.',
-								bgColor: '#0984e3',
-								textColor: 'white',
-								loaderBg: '#fdcb6e',
-								icon: 'info'
-							})
-					        ini.parents('.btn-group').html('<button class="btn btn-success btn-enable" type="button" title="Enable"><i class="fa fa-check-circle"></i></button>');
-				        }
-			        },
-			        cancel:{
-			        	text: 'Tidak',
-					    action: function () {
-    			            // tutup confirm
-    			        }
-    			    }
-			    }
-			});
-		});
-
-		$(document).on('click', '.btn-enable', function(){
-			$.toast({
-				heading: 'Information',
-				text: 'Data Berhasil di Aktifkan.',
-				bgColor: '#0984e3',
-				textColor: 'white',
-				loaderBg: '#fdcb6e',
-				icon: 'info'
-			})
-			$(this).parents('.btn-group').html('<button class="btn btn-warning btn-edit" type="button" title="Edit"><i class="fa fa-pencil"></i></button>'+
-	                                		'<button class="btn btn-danger btn-disable" type="button" title="Disable"><i class="fa fa-times-circle"></i></button>')
-		})
-
-		// function table_hapus(a){
-		// 	table.row($(a).parents('tr')).remove().draw();
-		// }
 	});
+
+	function detailReturn(id, detail) {
+        $("#detailReturn").modal("show");
+    }
+
+    function editReturn(id, detail) {
+
+    }
+
+    function hapusReturn(id, detail) {
+
+    }
 </script>
 @endsection
