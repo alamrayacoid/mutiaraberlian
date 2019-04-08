6b95a68ee03a0453d0dcab937d9e51c2fd68201c
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-04-08 10:59:22 +0700

    mutiara

--- resources/views/marketing/konsinyasipusat/index.blade.php
+++ resources/views/marketing/konsinyasipusat/index.blade.php
@@ -73,6 +73,10 @@
 
 		table_pus = $('#table_monitoringpenjualan').DataTable();
 
+        $('#detail-monitoring').DataTable( {
+            "iDisplayLength" : 5
+        });
+
 		$(document).on('click', '.btn-disable-pp', function(){
 			var ini = $(this);
 			$.confirm({
@@ -121,12 +125,6 @@
 			$(this).parents('.btn-group').html('<button class="btn btn-warning btn-edit" type="button" title="Edit"><i class="fa fa-pencil"></i></button>'+
 	                                		'<button class="btn btn-danger btn-disable" type="button" title="Disable"><i class="fa fa-times-circle"></i></button>')
 		})
-
-		$(document).ready(function() {
-			$('#detail-monitoring').DataTable( {
-				"iDisplayLength" : 5
-			});
-		});
 
 		$(document).on('click', '.btn-submit', function(){
 			$.toast({
