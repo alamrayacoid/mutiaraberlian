cd36d76bbd52e391b78d7de2c6ac9ee0885a4ce7
David Ridwan <ddavidridwan@gmail.com>
2019-04-07 20:06:09 -0700

    david

--- resources/views/sdm/prosesrekruitmen/rekrutmen/modal_view_img.blade.php
+++ resources/views/sdm/prosesrekruitmen/rekrutmen/modal_view_img.blade.php
@@ -5,7 +5,7 @@
     <div class="modal-content border-0" style="background-color:transparent;">
       <div style="background-color:transparent;">
         <button type="button" class="close" data-dismiss="modal" style="padding:10px; color:white; opacity:unset;" title="Exit">&times;</button>
-        <button id="button" class="btn btn-secondary btn-sm" style="float:right; background-color:transparent; border:none; color:white; margin-top:11px;" title="Rotate"><i class="fa fa-repeat" aria-hidden="true"></i></button>
+        <button id="button" class="btn btn-secondary btn-sm" style="float:right; background-color:transparent; border:none; color:white; margin-top:11px; outline:unset; box-shadow:unset;" title="Rotate"><i class="fa fa-repeat" aria-hidden="true"></i></button>
       </div>
       <div class="modal-body p-0 col-12">
         <section>
