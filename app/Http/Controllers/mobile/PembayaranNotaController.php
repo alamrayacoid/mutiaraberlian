98bf6545da4db6e026366eb6fce34aaa0526ca84
ilhamroliz <ilham.rolis@gmail.com>
2019-05-02 14:09:44 +0700

    roliz

--- app/Http/Controllers/mobile/PembayaranNotaController.php
+++ app/Http/Controllers/mobile/PembayaranNotaController.php
@@ -13,8 +13,9 @@ class PembayaranNotaController extends Controller
         $data = DB::table('d_productionorder')
             ->join('d_productionorderpayment', 'pop_productionorder', '=', 'po_id')
             ->join('m_supplier', 's_id', '=', 'po_supplier')
-            ->select('s_name', DB::raw('date_format(pop_datetop, "%d-%m-%Y") as pop_date'), DB::raw('replace(format(round(pop_value),0), ",", ".") as pop_value'))
+            ->select('s_company as s_name', 'po_nota', DB::raw('date_format(pop_datetop, "%d-%m-%Y") as pop_date'), DB::raw('replace(format(round(pop_value),0), ",", ".") as pop_value'))
             ->where('po_status', '=', 'BELUM')
+            ->orderBy('pop_date', 'desc')
             ->get();
 
         return json_encode(["NotaOrderProduksi" => $data]);
