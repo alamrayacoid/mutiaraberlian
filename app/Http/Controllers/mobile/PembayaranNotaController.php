0c665b6fdfb0ed4c931f58e86c50c53c303e8573
ilhamroliz <ilham.rolis@gmail.com>
2019-05-02 13:35:23 +0700

    roloiz;

--- app/Http/Controllers/mobile/PembayaranNotaController.php
+++ app/Http/Controllers/mobile/PembayaranNotaController.php
@@ -11,12 +11,12 @@ class PembayaranNotaController extends Controller
     public function getSupplier()
     {
         $data = DB::table('d_productionorder')
+            ->join('d_productionorderpayment', 'pop_productionorder', '=', 'po_id')
             ->join('m_supplier', 's_id', '=', 'po_supplier')
-            ->select( 's_name', DB::raw('count(po_supplier) as jumlah'))
+            ->select('s_name', DB::raw('date_format(pop_datetop, "%d-%m-%Y") as pop_date'), DB::raw('replace(format(round(pop_value),0), ",", ".") as pop_value'))
             ->where('po_status', '=', 'BELUM')
-            ->groupBy('po_supplier')
             ->get();
 
-        return json_encode($data);
+        return json_encode(["NotaOrderProduksi" => $data]);
     }
 }
