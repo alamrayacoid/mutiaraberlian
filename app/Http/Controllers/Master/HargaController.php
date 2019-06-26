66508c39b42ee7d090ebe7c0d25316caab6a4681
roliz <ilham.rolis@gmail.com>
2019-06-26 11:11:21 +0700

    'rolis'

--- app/Http/Controllers/Master/HargaController.php
+++ app/Http/Controllers/Master/HargaController.php
@@ -720,7 +720,7 @@ class HargaController extends Controller
             return response()->json(['status' => "Failed"]);
         }
         DB::beginTransaction();
-        //try {
+        try {
             if ($request->jenishargaHPA == "U") {
 
                 $check = DB::table('d_salespriceauth')
@@ -742,7 +742,7 @@ class HargaController extends Controller
                 } else {
                     $checkGol1 = DB::table('d_salespricedt')->where('spd_salesprice', '=', $idGol)->count();
                     $checkGol2 = DB::table('d_salespriceauth')->where('spa_salesprice', '=', $idGol)->count();
-
+                    $detailid = 1;
                     if ($checkGol1 > 0 && $checkGol2 > 0) {
                         $tmp_detail1 = DB::table('d_salespricedt')->where('spd_salesprice', '=', $idGol)->max('spd_detailid');
                         $tmp_detail2 = DB::table('d_salespriceauth')->where('spa_salesprice', '=', $idGol)->max('spa_detailid');
@@ -850,10 +850,10 @@ class HargaController extends Controller
                     return response()->json(['status' => "Success"]);
                 }
             }
-//        } catch (\Exception $e) {
-//            DB::rollback();
-//            return response()->json(['status' => "Failed", 'message' => $e]);
-//        }
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['status' => "Failed", 'message' => $e]);
+        }
     }
 
     public function deleteGolonganHarga($id, $detail, $status)
