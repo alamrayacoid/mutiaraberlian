c7de8e48a726a8d75a449250266b628a49e0b23e
swamsid <swamsid@gmail.com>
2019-07-03 16:55:48 +0700

    cek

--- app/Http/Controllers/Keuangan/transaksi/mutasi_kas/mutasi_kas_controller.php
+++ app/Http/Controllers/Keuangan/transaksi/mutasi_kas/mutasi_kas_controller.php
@@ -11,7 +11,39 @@ use Session;
 
 class mutasi_kas_controller extends Controller
 {
-    public function create(){
+    protected function create(){
     	return view('keuangan.transaksi.mutasi_kas.create');
     }
+
+    protected function resource(){
+    	$akun = DB::table('dk_akun')
+    				->where('ak_comp', Auth::user()->u_company)
+    				->where('ak_setara_kas', 1)
+    				->select('ak_id as id', 'ak_id', DB::raw('concat(ak_nomor, " - ", ak_nama) as text'))->get();
+
+    	return json_encode([
+    		"akun"	=> $akun
+    	]);
+    }
+
+    protected function save(Request $request){
+    	return json_encode($request->all());
+
+    	DB::beginTransaction();
+
+    	try {
+    		
+    		
+    		
+    	} catch (Exception $e) {
+    		DB::rollback();
+
+            return json_encode([
+                'status'  => 'error',
+                'text'    => 'System Mengalami Masalah. '.$e
+            ]);
+    	}
+    }
+
+
 }
