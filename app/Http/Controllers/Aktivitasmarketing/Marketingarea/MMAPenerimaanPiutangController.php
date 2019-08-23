289a185777f2bd9e1ecf22d777d4726c9f4961f0
Rizqon <m.rum.rowi.r@gmail.com>
2019-08-23 11:25:12 +0700

    up minor

--- app/Http/Controllers/Aktivitasmarketing/Marketingarea/MMAPenerimaanPiutangController.php
+++ app/Http/Controllers/Aktivitasmarketing/Marketingarea/MMAPenerimaanPiutangController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Crypt;
 
+use App\Model\keuangan\dk_akun;
 use App\d_salescomp;
 use App\m_paymentmethod;
 use Auth;
