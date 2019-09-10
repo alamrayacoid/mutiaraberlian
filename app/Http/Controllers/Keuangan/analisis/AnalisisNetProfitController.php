03b6321561facfef63267102f89fd70e9480aa30
roliz <ilham.rolis@gmail.com>
2019-09-10 14:07:14 +0700

    ok

--- app/Http/Controllers/Keuangan/analisis/AnalisisNetProfitController.php
+++ app/Http/Controllers/Keuangan/analisis/AnalisisNetProfitController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Keuangan\analisis;
 
+use Auth;
+use DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +11,11 @@ class AnalisisNetProfitController extends Controller
 {
     public function index()
     {
-        return view('keuangan.analisis.netprofit.index');
+        $cabang = json_encode(DB::table('m_company')
+            ->where('c_id', Auth::user()->u_company)
+            ->select('c_id as id', 'c_name as text')
+            ->get());
+
+        return view('keuangan.analisis.netprofit.index', compact('cabang'));
     }
 }
